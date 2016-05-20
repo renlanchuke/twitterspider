6bf8180e2138be849c0619556d012b582d7868db
renlanchuke <1065730562@qq.com>
2016-05-20 21:10:01 +0800

    getTwitterId

--- common.js
+++ common.js
@@ -1,3 +1,66 @@
+var fs = require('fs');
+var http = require("http");
+var request = require('request');
+var zlib = require('zlib');
+var config = require("./config");
+var logger = require("./logger");
+
+
+
+//////////////////网络访问部分
+
+var maxretry = 2;//请求如果出错的话，最大重试次数
+
+//使用cookie访问页面
+exports.get = function (url, cookie, callback, retry) {
+    if (!retry) retry = 0;
+    var headers = {
+        'Accept': 'text/html, application/xhtml+xml, */*',
+        'Accept-Language': 'zh-CN',
+        'User-Agent': 'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; WOW64; Trident/6.0)',
+        'Connection': 'Keep-Alive',
+        'Accept-Encoding': 'gzip,deflate',     
+        'auth_token':'778DF829AAD63C67ED4CC77F63B0B4E5DA8064FA'
+    };
+
+    request({
+        url: url,
+        headers: headers,
+        timeout: 15000,
+        encoding: null
+    },
+        function (error, response, data) {
+           // console.log("error: "+error);
+            if (!error && response.statusCode == 200) {
+
+                var buffer = new Buffer(data);
+                var encoding = response.headers['content-encoding'];
+                if (encoding == 'gzip') {
+                    zlib.gunzip(buffer, function (err, decoded) {
+                        //console.log("err: "+ err);
+                        callback(err && ('unzip error' + err), decoded && decoded.toString());
+                    });
+                } else if (encoding == 'deflate') {
+                    zlib.inflate(buffer, function (err, decoded) {
+                        callback(err && ('deflate error' + err), decoded && decoded.toString());
+                    })
+                } else {
+
+                    callback(null, buffer.toString());
+                }
+            }
+            else {
+                //小于错误次数则重试
+                if (retry < maxretry) {
+                    logger.debug("retry getting url : " + url);
+                    self.get(url, cookie, callback, retry + 1);
+                }
+                else
+                    callback(error || response.statusCode);
+            }
+        });
+}
+
 //时间格式化输出
 exports.getDateString = function (date) {
     if (!date) {
--- getTwitterID.js
+++ getTwitterID.js
@@ -1,12 +1,32 @@
-var retry = 3;
-gettwitterid(last_note_ts, max_position);
+var common = require('./common');
+var mongo = require('./mongoDB');
+var cheerio = require('cheerio');
+var logger = require('./logger');
+
+var last_note_ts = 1463307756;
+var max_position = 'max_position=TWEET-688517798804496384-688520937892929536-BD1UO2FFu9QAAAAAAAAETAAAAAcAAAASAAAAAA' +
+    'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA' +
+    'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA';
+var URL = 'https://twitter.com/i/search/timeline?vertical=default&' +
+    'q=%E8%94%A1%E8%8B%B1%E6%96%87%20lang%3Azh%20since%3A2016-01-14%20until%3A2016-01-17&src=typd&' +
+    'include_available_features=1&include_entities=1&last_note_ts=' + last_note_ts + '&' + max_position +
+    '&reset_error_state=false';
+
+var query = 'https://twitter.com/i/search/timeline?vertical=default&' +
+    'q=%E8%94%A1%E8%8B%B1%E6%96%87%20lang%3Azh%20since%3A2016-01-14%20until%3A2016-01-17&src=typd&' +
+    'include_available_features=1&include_entities=1&last_note_ts=';
+var lpara = '&reset_error_state=false';
+
+
+mongo.init((err) => {
+    if (err) throw err;
+    gettwitterid(last_note_ts, max_position);
+
+});
 
 function gettwitterid(lastNoteTS, maxPosition) {
-    if (retry === 0)
-        return;
-
     var currURL = query + lastNoteTS + '&' + maxPosition + '&' + lpara;
-    tools.get(currURL, null, (err, data) => {
+    common.get(currURL, null, (err, data) => {
         if (err) throw err;
         var json = JSON.parse(data);
         if (json.new_latent_count < 1)
@@ -14,16 +34,19 @@ function gettwitterid(lastNoteTS, maxPosition) {
         var twitterIDArray = new Array();
         var $ = cheerio.load(json.items_html, { decodeEntities: false });
         var cards = $('.js-original-tweet');
+
         cards.each(function (i) {
             var id = $(this).data('tweet-id');
             twitterIDArray.push({ twitter_id: id, download: false });
-        });
-        console.log(twitterIDArray)
+        }),
+            mongo.insertMany('twitterID', twitterIDArray, (err, result) => {
+                logger.log('成功插入' + result.insertedCount + '条数据');
+            });
+        // console.log(twitterIDArray)
         var currLastNoteTS = lastNoteTS + 4;
         var currMaxPosition = 'max_position=' + json.min_position;
-        console.log(currLastNoteTS);
-        console.log(currMaxPosition);
-        retry--;
+        // console.log(currLastNoteTS);
+        // console.log(currMaxPosition);
         gettwitterid(currLastNoteTS, currMaxPosition);
     });
 }
