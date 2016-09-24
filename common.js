0dda603947ecb95f799f7280170a32afb04a7893
renlanchuke <1065730562@qq.com>
2016-09-24 20:56:33 +0800

    some parameter

--- common.js
+++ common.js
@@ -28,7 +28,7 @@ exports.get = function (url, cookie, callback, retry) {
     request({
         url: url,
         headers: headers,
-        timeout: 15000,
+        timeout: 3000,
         encoding: null
     },
         function (error, response, data) {
@@ -69,17 +69,21 @@ exports.get = function (url, cookie, callback, retry) {
 //用form訪問
 exports.getContent = function (url, form, callback, retry) {
     if (!retry) retry = 0;
-    var headers = {
-        'Accept': 'text/html, application/xhtml+xml, */*',
-        'Accept-Language': 'zh-CN',
-        'User-Agent': 'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; WOW64; Trident/6.0)',
-        'Connection': 'Keep-Alive',
-        'Accept-Encoding': 'gzip,deflate'
-        //'auth_token': '778DF829AAD63C67ED4CC77F63B0B4E5DA8064FA'
+    var header = {
+        'Host': 'search.appledaily.com.tw',
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:49.0) Gecko/20100101 Firefox/49.0',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+        'Accept-Language': 'en-US,en;q=0.5',
+        'Accept-Encoding': 'gzip, deflate',
+        'Referer': 'http://search.appledaily.com.tw/appledaily/search',
+        'Cookie': '__gads=ID=1d16a7619e04b96c:T=1474698703:S=ALNI_MaCy_qdMc9zcjAclzM-0Fl2x904Kg; _ga=GA1.3.1268670537.1474698703; _parsely_visitor={%22id%22:%222b73725c-55f7-45c9-ae66-530323a3fef0%22%2C%22session_count%22:4%2C%22last_session_ts%22:1474718566380}; ZP_CAL=%27fdow%27%3Anull%2C%27history%27%3A%222016/01/15/00/00%2C2015/12/14/00/00%22%2C%27sortOrder%27%3A%22asc%22%2C%27hsize%27%3A9; _parsely_session={%22sid%22:4%2C%22surl%22:%22http://www.appledaily.com.tw/appledaily/article/headline/20160924/37393667/applesearch/%25E8%2594%25A1%25EF%25BC%259A%25E9%25A3%259B%25E5%25AE%2589%25E4%25B8%258D%25E6%2587%2589%25E8%25A2%25AB%25E6%2594%25BF%25E6%25B2%25BB%25E4%25BB%258B%25E5%2585%25A5%22%2C%22sref%22:%22http://search.appledaily.com.tw/appledaily/search%22%2C%22sts%22:1474718566380%2C%22slts%22:1474714129813}; _gali=pageNumber',
+        'Connection': 'keep-alive',
+        'Upgrade-Insecure-Requests': '1'
     };
 
     request.post({
         url: url,
+        header,
         form: form
         // headers: headers,
         // timeout: 15000,
--- getArticlesFromAppleNews.js
+++ getArticlesFromAppleNews.js
@@ -5,6 +5,7 @@ var logger = require('./logger');
 
 var fs = require('fs')
 var url = "http://search.appledaily.com.tw/appledaily/search";
+//第一次搜索的表单
 var formData = {
     searchMode: "Adv",
     searchType: "text",
@@ -15,23 +16,24 @@ var formData = {
     edate: "2016-01-15"
 };
 
-var page=1;
+var page = 1;
+//分页链接的表单数据
 var formData2 = {
-    searchMode: "Adv",
+    searchMode: "",
     searchType: "text",
-    ExtFilter:"[twapple+TO+twapple_sub]",
-    sorttype:"1",
-    ketword:"蔡英文",
-    rangedate:"[20151214+TO+20160115999:99]",
-    totalpage:"276",
-    page:page
+    ExtFilter: "[twapple+TO+twapple_sub]",
+    sorttype: "1",
+    ketword: "蔡英文",
+    rangedate: "[20151214+TO+20160115999:99]",
+    totalpage: "276",
+    page: '7'
 };
 
 
 exports.getArticles = function () {
     common.getContent(url, formData, function (err, fileContent) {
         if (err) throw err;
-        fs.writeFile("./testFile", fileContent, (err) => {
+        fs.writeFile("./testFile2", fileContent, (err) => {
             if (err) throw err;
             console.log('It\'s saved!');
         });
